b4d3def6591b34098aebf0f1fbfb5c3c87d5ec6e
golamrabbiazad <golamrabbiazad@gmail.com>
2021-05-13 03:24:14 +0600

    syntax hightlights for mdx

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
-import { Table } from "./src/components"
-import Theme from "./src/themes/theme"
 import { MDXProvider } from "@mdx-js/react"
+import { preToCodeBlock } from "mdx-utils"
+import { Table, Code } from "./src/components"
+import Theme from "./src/themes/theme"
+import "./language-tabs.css"
 
 const GlobalStyles = createGlobalStyle`
     * {
@@ -19,6 +21,17 @@ const GlobalStyles = createGlobalStyle`
 
 export const components = {
   table: Table,
+  pre: preProps => {
+    const props = preToCodeBlock(preProps)
+
+    if (props) {
+      return <Code {...props} />
+    }
+
+    return <pre {...preProps} />
+  },
+
+  wrapper: ({ children }) => <>{children}</>,
 }
 
 export const wrapRootElement = ({ element }) => (
