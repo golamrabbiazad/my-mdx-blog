6f402d13e07d08192e89b7ca3b568085f083f206
golamrabbiazad <golamrabbiazad@gmail.com>
2021-05-13 04:02:53 +0600

    404 added

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
 import React from "react"
+import { graphql } from "gatsby"
 import { H1 } from "../styles"
+import { Container, Content, FeatureImage } from "../components"
 
-const NotFound = () => {
+const NotFound = ({ data }) => {
+  const featureImage = data.file.childImageSharp.gatsbyImageData
+
   return (
-    <div>
-      <H1 style={{ textAlign: "center", margin: "40px auto" }}>
-        Your content isn't here!
-      </H1>
-    </div>
+    <Container>
+      <FeatureImage fixed={featureImage} />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem">
+          404! Not Found
+        </H1>
+      </Content>
+    </Container>
   )
 }
+
+export const notFoundQuery = graphql`
+  query NotFoundQuery {
+    file(relativePath: { eq: "sorry-404.jpg" }) {
+      childImageSharp {
+        gatsbyImageData(layout: FIXED)
+      }
+    }
+  }
+`
 
 export default NotFound
